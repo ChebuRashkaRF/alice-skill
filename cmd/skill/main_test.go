d1ce629dba0a08490ebe3f843af45dc086a1fc98
Zhurbenko Maksim <zhurbenkoma97@mail.ru>
2024-04-10 21:18:09 +0300

    test(TestWebhook): добавил тесты, обновил зависимости

--- cmd/skill/main_test.go
+++ cmd/skill/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/go-resty/resty/v2"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,6 +10,14 @@ import (
 )
 
 func TestWebhook(t *testing.T) {
+	// тип http.HandlerFunc реализует интерфейс http.Handler
+	// это поможет передать хендлер тестовому серверу
+	handler := http.HandlerFunc(webhook)
+	// запускаем тестовый сервер, будет выбран первый свободный порт
+	srv := httptest.NewServer(handler)
+	// останавливаем сервер после завершения теста
+	defer srv.Close()
+
 	// описываем ожидаемое тело ответа при успешном запросе
 	successBody := `{
         "response": {
@@ -31,17 +40,20 @@ func TestWebhook(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.method, func(t *testing.T) {
-			r := httptest.NewRequest(tc.method, "/", nil)
-			w := httptest.NewRecorder()
+			// делаем запрос с помощью библиотеки resty к адресу запущенного сервера,
+			// который хранится в поле URL соответствующей структуры
 
-			// вызовем хендлер как обычную функцию, без запуска самого сервера
-			webhook(w, r)
+			req := resty.New().R()
+			req.Method = tc.method
+			req.URL = srv.URL
 
-			assert.Equal(t, tc.expectedCode, w.Code, "Код ответа не совпадает с ожидаемым")
-			// проверим корректность полученного тела ответа, если мы его ожидаем
+			resp, err := req.Send()
+			assert.NoError(t, err, "error making HTTP request")
+
+			assert.Equal(t, tc.expectedCode, resp.StatusCode(), "Response code didn't match expected")
+			// проверяем корректность полученного тела ответа, если мы его ожидаем
 			if tc.expectedBody != "" {
-				// assert.JSONEq помогает сравнить две JSON-строки
-				assert.JSONEq(t, tc.expectedBody, w.Body.String(), "Тело ответа не совпадает с ожидаемым")
+				assert.JSONEq(t, tc.expectedBody, string(resp.Body()))
 			}
 		})
 	}
--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.22.1
 
 require github.com/stretchr/testify v1.9.0
 
+require github.com/go-resty/resty/v2 v2.12.0
+
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
