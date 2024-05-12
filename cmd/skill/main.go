43f0e1d5c4af9ac0cbe0021822eb1e3354a3df8c
Zhurbenko Maksim <zhurbenkoma97@mail.ru>
2024-05-12 21:35:08 +0300

    feat(log): добавил бибдиотеку zap для логирования

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -4,6 +4,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"go.uber.org/zap"
+
+	"github.com/ChebuRashkaRF/alice-skill/internal/logger"
 )
 
 // функция main вызывается автоматически при запуске приложения
@@ -17,13 +21,20 @@ func main() {
 
 // функция run будет полезна при инициализации зависимостей сервера перед запуском
 func run() error {
+	if err := logger.Initialize(flagLogLevel); err != nil {
+		return err
+	}
+
+	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
+
 	fmt.Println("Running server on", flagRunAddr)
-	return http.ListenAndServe(flagRunAddr, http.HandlerFunc(webhook))
+	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(webhook))
 }
 
 // функция webhook — обработчик HTTP-запроса
 func webhook(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
+		logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
 		// разрешаем только POST-запросы
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
@@ -40,6 +51,7 @@ func webhook(w http.ResponseWriter, r *http.Request) {
         "version": "1.0"
       }
     `))
+	logger.Log.Debug("sending HTTP 200 response")
 }
 
 // tic-tac
