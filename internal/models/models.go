d45bfd70184cbec508518ab21b7e876d332f3dfb
Zhurbenko Maksim <zhurbenkoma97@mail.ru>
2024-07-21 01:45:18 +0300

    feat(Request): добавил новые поля Timezone и Session

--- internal/models/models.go
+++ internal/models/models.go
@@ -7,14 +7,20 @@ const (
 // Request описывает запрос пользователя.
 // см. https://yandex.ru/dev/dialogs/alice/doc/request.html
 type Request struct {
-	Request SimpleUtterance `json:"request"`
-	Version string          `json:"version"`
+	Timezone string          `json:"timezone"`
+	Request  SimpleUtterance `json:"request"`
+	Session  Session         `json:"session"`
+	Version  string          `json:"version"`
 }
 
 // SimpleUtterance описывает команду, полученную в запросе типа SimpleUtterance.
 type SimpleUtterance struct {
 	Type    string `json:"type"`
 	Command string `json:"command"`
+}
+
+type Session struct {
+	New bool `json:"new"`
 }
 
 // Response описывает ответ сервера.
