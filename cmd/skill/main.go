015299909e46d7adee7371db3cc2565fe6a3a532
Zhurbenko Maksim <zhurbenkoma97@mail.ru>
2024-08-19 21:02:13 +0300

    feat: добавил реализацию субд PostgreSQL

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -2,11 +2,14 @@
 package main
 
 import (
-	"go.uber.org/zap"
+	"database/sql"
 	"net/http"
 	"strings"
 
 	"github.com/ChebuRashkaRF/alice-skill/internal/logger"
+	"github.com/ChebuRashkaRF/alice-skill/internal/store/pg"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"go.uber.org/zap"
 )
 
 func gzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
@@ -62,8 +65,14 @@ func run() error {
 		return err
 	}
 
-	// создаём экземпляр приложения, пока без внешней зависимости хранилища сообщений
-	appInstance := newApp(nil)
+	// создаём соединение с СУБД PostgreSQL с помощью аргумента командной строки
+	conn, err := sql.Open("pgx", flagDatabaseURI)
+	if err != nil {
+		return err
+	}
+
+	// создаём экземпляр приложения, передавая реализацию хранилища pg в качестве внешней зависимости
+	appInstance := newApp(pg.NewStore(conn))
 
 	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
 	// оборачиваем хендлер webhook в middleware с логированием и поддержкой gzip
